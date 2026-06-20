c9de63d5b5a87a902bcbbf6c53bb371476444c39
AlbanoRavenPOGI <onlyven711@gmail.com>
2026-06-20 14:29:54 +0800

    Commit Classes

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentManagementSystem
+{
+ 
+    public class Student
+    {
+        public string Name { get; set; }
+        public double Grade { get; set; }
+
+     
+        public Student(string name, double grade)
+        {
+            Name = name;
+            Grade = grade;
+        }
+    }
+    public class StudentManager
+    {
+        public List<Student> Students { get; private set; } = new List<Student>();
+
+        public void AddStudent(string name, double grade)
+        {
+            Students.Add(new Student(name, grade));
+        }
+
+        public double CalculateAverage()
+        {
+            if (Students.Count == 0) return 0;
+            double sum = 0;
+            foreach (var student in Students)
+            {
+                sum += student.Grade;
+            }
+            return sum / Students.Count;
+        }
+
+        public Student GetTopStudent()
+        {
+            if (Students.Count == 0) return null;
+            Student top = Students[0];
+            foreach (var student in Students)
+            {
+                if (student.Grade > top.Grade)
+                {
+                    top = student;
+                }
+            }
+            return top;
+        }
+    }
+}
+
